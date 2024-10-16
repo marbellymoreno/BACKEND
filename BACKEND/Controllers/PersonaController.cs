1999ca8ff5b2033c0c603caaa177c70313083270
marbellymoreno <me0763032023@unab.edu.sv>
2024-10-15 21:28:38 -0600

    Guía 8: Respuestas de dato especifico.

---
+++ BACKEND/Controllers/PersonaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+    }
+}
---
+++ BACKEND/Controllers/Repository.cs
@@ -0,0 +1,35 @@
+﻿namespace BACKEND.Controllers
+{
+    public class Repository
+    {
+        public static List<PersonaDatos> persona = new List<PersonaDatos> 
+        { 
+            new PersonaDatos()
+            {
+                id = 1,
+                age = new DateTime(1999,08,09),
+                name ="Delmy Michelle Moreno de Menjivar"
+            },
+            new PersonaDatos()
+            {
+                id = 2,
+                age = new DateTime(2023,10,31),
+                name ="Rocio Michelle Menjivar Moreno"
+            },
+            new PersonaDatos()
+            {
+                id = 3,
+                age = new DateTime(2005,10,31),
+                name ="Mathias Lisandro Menjivar Moreno"
+            }
+        };
+    }
+
+    public class PersonaDatos
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime age { get; set; }
+        public string email { get; set; }
+    }
+}
